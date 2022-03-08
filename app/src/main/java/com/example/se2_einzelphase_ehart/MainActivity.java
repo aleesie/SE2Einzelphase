4b8b04d9c8bac537a86e4283e2f43d3262c5fee5
Alina <alinaeh@edu.aau.at>
2022-03-08 17:49:17 +0100

    declaration of necessary variables

--- app/src/main/java/com/example/se2_einzelphase_ehart/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelphase_ehart/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.se2_einzelphase_ehart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button btn_send;
+    public EditText input_txt;
+    public TextView output_txt;
+    private String input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
